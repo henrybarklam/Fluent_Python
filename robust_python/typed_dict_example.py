845cece65511a5ac56e63e4931c03c1b8b07c5ab
Henry Barklam <henry@optoinvest.com>
2024-01-16 08:38:56 -0500

    Small comment addition

--- robust_python/typed_dict_example.py
+++ robust_python/typed_dict_example.py
@@ -1,6 +1,8 @@
 from typing import TypedDict
 
-# TypedDict is for the type checker only, will be converted to a dictionary regardless
+'''TypedDict is for the type checker only, will be converted to a dictionary regardless
+If the API called at the end ever changes, its easier for a developer to change the types etc
+'''
 class Range(TypedDict):
     min: float
     max: float
@@ -17,3 +19,9 @@ class RecipeNutritionInformation(TypedDict):
     fat: NutritionInformation
     protein: NutritionInformation
     carbs: NutritionInformation
+
+nutrition_infomation: RecipeNutritionInformation = someApiCall(recipe_name)
+
+def someApiCall(some_recipe):
+    # This would have to return a variable of type RecipeNutritionInformation
+    return
