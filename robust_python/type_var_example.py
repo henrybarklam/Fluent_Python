b2727f0231358dc221a8c6751b32db95257b424f
Henry Barklam <henry@optoinvest.com>
2024-01-16 08:51:48 -0500

    Small comment addition

--- robust_python/type_var_example.py
+++ robust_python/type_var_example.py
@@ -1,6 +1,11 @@
 from typing import TypeVar
 
-T = TypeVar('T')
+'''
+This says that for a type T, reverse takes in a list of elements of type T
+and returns a list of elements of type T - it can't mix types, a list of integers will never be able
+to become a list of strings if those lists aren't using the same TypeVar
+'''
 
+T = TypeVar('T')
 def reverse(coll: list[T]) -> list[T]:
     return coll[::-1]
